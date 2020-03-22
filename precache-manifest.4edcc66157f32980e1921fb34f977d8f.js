52d73d99e7b145a7941af3c50afeb91e480e75b2
Anton Kablukov <antoha7@gmail.com>
2020-03-22 21:13:40 +0100

    Updates

---
+++ precache-manifest.4edcc66157f32980e1921fb34f977d8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e0a99043d6157a72c290c82fb120a2",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "917c0b2c629ce9d6ec4f",
+    "url": "/react-movie-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3d7aa42673361c8c8fd6",
+    "url": "/react-movie-app/static/css/main.dee2d55a.chunk.css"
+  },
+  {
+    "revision": "917c0b2c629ce9d6ec4f",
+    "url": "/react-movie-app/static/js/2.52ea429f.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/react-movie-app/static/js/2.52ea429f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d7aa42673361c8c8fd6",
+    "url": "/react-movie-app/static/js/main.e430f5f6.chunk.js"
+  },
+  {
+    "revision": "98a0803a02abaab14463",
+    "url": "/react-movie-app/static/js/runtime-main.7ac0493a.js"
+  }
+]);
